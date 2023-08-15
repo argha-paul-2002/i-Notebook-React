087a496d01ec90992327c94ec1f54e05b2289819
Argha Paul <arghapaul002@gmail.com>
2023-08-15 12:27:52 +0530

    Data validation in backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+// Available Routes
+app.get('/', (req, res) => {
+  res.send('Hello Argha!')
+})
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})

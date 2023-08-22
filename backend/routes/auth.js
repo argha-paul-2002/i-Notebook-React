b37dc11f936412a9c24bbd3ac724aaf512b2e219
Argha Paul <atghapaul002@gmail.com>
2023-08-22 19:25:40 +0530

    Login page styling and logic build done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -64,6 +64,7 @@ router.post('/login',[
   body('password', 'Password can not be blank').exists(),
 ] ,async (req, res)=>{ 
 
+  let success = false;
   // If there are errors, return bad request ans the errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -74,13 +75,15 @@ router.post('/login',[
   try {
     let user = await User.findOne({email});
     if(!user){
-      return res.status(400).json({error: "Please try to login with correct credentials"});
+      success = false;
+      return res.status(400).json({success, error: "Please try to login with correct credentials"});
     }
 
     const passwordCompare =await bcrypt.compare(password, user.password);
 
     if(!passwordCompare){
-      return res.status(400).json({error: "Please try to login with correct credentials"});
+      success = false;
+      return res.status(400).json({success, error: "Please try to login with correct credentials"});
     }
 
     const data = {
@@ -89,7 +92,8 @@ router.post('/login',[
       }
     }
     const authtoken = jwt.sign(data, JWT_SECRET);
-    res.json({authtoken})
+    success = true;
+    res.json({success, authtoken})
 
   } // Catch error
   catch (error) {

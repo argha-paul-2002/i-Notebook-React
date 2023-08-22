db91b3bb9e378002823138b65753e16cf5ec167d
Argha Paul <arghapaul002@gmail.com>
2023-08-22 14:05:00 +0530

    fetch from backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Route,  Routes} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
+import Alert from './components/Alert';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+    <NoteState>
+      <BrowserRouter>
+        <Navbar />
+        <Alert message="React Course" />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/about" element={<About/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </NoteState>
+    </>
   );
 }
 
